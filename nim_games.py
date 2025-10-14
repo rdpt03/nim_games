bca447c7e954f8463c94b0280930406c2b4afcda
Rafael DA SILVA <rafael1893porto@gmail.com>
2025-10-14 13:45:34 +0200

    added bot player

--- nim_games.py
+++ nim_games.py
@@ -7,26 +7,43 @@ Jeux  de Nim (variante simple et de Marienbad)
 if __name__ == '__main__':
     #game vars
     matchsticks = 21
-
+    last_amount_to_remove = 0
     #ask player details
-    player1 = input("Insert player 1 name : ")
-    player2 = input("Insert player 2 name : ")
-    players = {1:player1,2:player2}
+    player1 = input("Insert player 1 name ('ordinateur' to play against the computer): ")
+    player2 = input("Insert player 2 name ('ordinateur' to play against the computer): ")
 
+    #check if both players have the same name
+    #if player1 == player2:
+    #    print("both players cannot have the same name")
+    #    exit()
+    players = [{'player_n':1, 'name':player1},{'player_n':2,'name':player2}]
+
+    turn = 1
     game_over = False
     #game
     while not game_over:
         #play player 1 or 2
-        for player_num in [1,2]:
+        for player in players:#[1,2]:
             while True:
                 #printing area
                 print('-'*30)
-                print(f"{players[player_num]}'s turn")
+                print(f"{player['name']}'s turn")
                 print(f"{matchsticks} matchsticks remaining in the game")
 
-                #amount to remove
-                amount_to_remove = input("Insert the amount of matchsticks to remove (1-4): ")
+                #amount to remove (if computer is diferent)
+                #computer as player 1
+                if player['name'].lower() == 'ordinateur' and player['player_n'] == 1 and turn == 1:
+                    amount_to_remove = str(matchsticks % 5)
+                    print(f'-> Computer removed {amount_to_remove} matchsticks')
+                #computer as player 2
+                elif player['name'].lower() == 'ordinateur' and (player['player_n'] == 2 or not turn == 1):
+                    amount_to_remove = str(5 - last_amount_to_remove)
+                    print(f'-> Computer removed {amount_to_remove} matchsticks')
+                #regular player
+                else:
+                    amount_to_remove = input("Insert the amount of matchsticks to remove (1-4): ")
 
+                turn +=1
                 #check if amount is a digit
                 if amount_to_remove.isdigit():
                     amount_to_remove = int(amount_to_remove)
@@ -34,11 +51,11 @@ if __name__ == '__main__':
                     if 1 <= amount_to_remove <= 4:
                         # remove matchsticks
                         matchsticks -= amount_to_remove
-
+                        last_amount_to_remove = amount_to_remove
 
                         # check if last was removed
                         if matchsticks <= 0:
-                            print(f"{players[player_num]} took the last matchstick and lost the game 😢")
+                            print(f"{player['name']} (player {player['player_n']}) took the last matchstick and lost the game 😢")
                             game_over = True
                             break
                         else:
